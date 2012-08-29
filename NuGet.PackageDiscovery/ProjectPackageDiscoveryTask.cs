b6fc2cc424370fd975fdc7d0ab8bbfeb02e34156
Jeff Handley <jeff@jeffhandley.com>
2012-08-28 23:14:07 -0700

    Creating the NuGet.PackageDiscovery project for emitting package folders as build output

---
+++ NuGet.PackageDiscovery/ProjectPackageDiscoveryTask.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+
+namespace NuGet.PackageDiscovery
+{
+    public class ProjectPackageDiscoveryTask : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            var packagesConfig = Path.Combine(this.MSBuildProjectDirectory, "packages.config");
+
+            var packagesFolder = Path.Combine(this.SolutionDir, "packages");
+            var packagesRepo = new SharedPackageRepository(packagesFolder);
+            var projectPackages = new PackageReferenceRepository(new PhysicalFileSystem(this.MSBuildProjectDirectory), packagesRepo);
+
+            projectPackages.GetPackages().Select(p => packagesRepo.PathResolver.GetPackageDirectory(p)).ToList().ForEach(p => _packageFolders.Add(Path.Combine(packagesFolder, p)));
+            return true;
+        }
+
+        private IList<string> _packageFolders = new List<string>();
+
+        public string MSBuildProjectDirectory { get; set; }
+        public string SolutionDir { get; set; }
+
+        [Output]
+        public string[] PackageFolders
+        {
+            get
+            {
+                return _packageFolders.ToArray();
+            }
+        }
+    }
+}
